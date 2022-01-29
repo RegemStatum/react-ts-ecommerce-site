91114be47920a0cba631060592aab9f75e1b8e78
Oleksandr Kondratov <19samsung81k9@gmail.com>
2022-01-29 09:12:31 +0200

    fonts check

---
+++ src/components/pages/home/Hero.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const Hero: FC = () => {
+  return (
+    <div className="hero">
+      <p>12</p>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from "react";
 import { useAppContext } from "../context/AppContext";
 import appContextValueType from "../types/appContextValue";
+import { Hero } from "../components/pages/home";
 
 const HomePage: FC = () => {
   return (
     <>
       <div className="container">
-        <h3>Home page</h3>
+        <Hero />
       </div>
     </>
   );
